df1706c13040d19cb8b2385a4e670970e17b30d2
Uthrasarathi <uthrasarathi1@gmail.com>
2016-06-29 09:31:39 +0530

    Create k2.c

---
+++ k2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number\n");
+scanf("%d",&num);
+if((num%2)==0)
+{
+printf("even number\n");
+}
+else
+{
+printf("odd number\n");
+}
+getch();
+}
